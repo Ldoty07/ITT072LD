a32ae643cbf3520d6be67d1b0c04e4ade0344a5b
Ldoty07 <ldoty2@lakelandcollege.edu>
2026-09-17 11:22:42 -0500

    fix links to data.js and render.js in main.js

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@
 import './src/style.css'
 
 // Import render functions
-import { renderGoals } from './render.js'
-import { renderSkillsIHave } from './render.js'
-import { renderSkillsToLearn } from './render.js'
+import { renderGoals } from './src/render.js'
+import { renderSkillsIHave } from './src/render.js'
+import { renderSkillsToLearn } from './src/render.js'
 
 // Import data
-import { goals } from './data.js'
-import { skillsIHave } from './data.js'
-import { skillsToLearn } from './data.js'
+import { goals } from './src/data.js'
+import { skillsIHave } from './src/data.js'
+import { skillsToLearn } from './src/data.js'
 
 // Define containers
 const goalsContainer = document.querySelector("#goalsContainer");
