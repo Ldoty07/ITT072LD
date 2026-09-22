f3a1c400d97b1ad78d3f1693848c911587e3197e
Ldoty07 <ldoty2@lakelandcollege.edu>
2026-09-22 13:22:31 -0500

    change base path

--- react-restaurant-menu/vite.config.js
+++ react-restaurant-menu/vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ITT072LD/',
+  base: '/ITT072LD/react-restaurant-menu/',
 })
